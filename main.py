9b4ac689d69c683c2d9a09bd2a5c76f2200801ec
Faris Siddiqi <farisiddiqi@gmail.com>
2024-01-01 13:58:00 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,74 @@
+from hand_tracking_module import handDetector
+from volume_control import AudioController
+import cv2
+import time
+import math
+
+
+
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    audio = AudioController()
+    while True:
+        # save the frame of the video cap to img 
+        success, img = cap.read()
+
+        img = detector.findHands(img)
+        lmList = detector.findPos(img)
+        if len(lmList) != 0:
+            # get x, y positions of thumb 
+            thumb = lmList[4]
+            # get x, y positions of finger 
+            index = lmList[8]
+            # get distance of the two
+            distance = get_distance(thumb, index)
+            print(distance)
+            # conver the distance to a percentage
+            percent = convert_to_percent(distance)
+            # change audo to said percentage 
+            audio.change_volume(percent)
+
+            # present current volume on screen
+            cv2.putText(img, str(audio.get_volume()), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+
+        cv2.imshow("Image", img)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+
+
+
+
+def get_distance(thumb, index):
+    # param: x,y cords of thumb and index 
+    # use distance formula to calculate the distance between thumb and finger 
+    # return distance 
+    x, y = thumb[1], thumb[2]
+    x2, y2 = index[1], index[2]
+
+    distance = math.sqrt(math.pow(x2-x,2) + math.pow(y2-y,2))
+
+    return distance
+
+
+
+
+def convert_to_percent(distance):
+    # param distance of the thumb and index 
+    # turn the distance into a percentage 0-100 so it can be used to change the volume 
+    # return percentage 
+
+    if distance > 100:
+        return 100
+    percent = int(distance)
+    return percent
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ volume_control.py
@@ -0,0 +1,23 @@
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+class AudioController:
+    def __init__(self):
+        # Initialize the audio controller with the default audio device
+        self.devices = AudioUtilities.GetSpeakers()
+        self.interface = self.devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        self.volume = cast(self.interface, POINTER(IAudioEndpointVolume))
+
+    def change_volume(self, new_volume):
+        # Set the master volume level to new_volume
+        self.volume.SetMasterVolumeLevelScalar(new_volume / 100.0, None)
+
+        # return new volume 
+        cur_volume = int(self.volume.GetMasterVolumeLevelScalar() * 100)
+        return cur_volume
+
+    def get_volume(self):
+        return int(self.volume.GetMasterVolumeLevelScalar() * 100)
+    
+
